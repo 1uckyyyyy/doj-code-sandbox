41773d905af11b4bbe4b190bf7b86d82cf0e9959
djay <2567645235@qq.com>
2024-04-26 16:07:47 +0800

    first commit

---
+++ src/main/java/com/djay/dojcodesandbox/DojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.djay.dojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DojCodeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/djay/dojcodesandbox/DojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djay.dojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
